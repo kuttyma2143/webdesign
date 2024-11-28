8fc2a89a66d8e1cb65f101eb10198628e081d8dc
kuttyma2143 <arafath5023gmail.com>
2024-11-29 01:09:36 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+import "./App.css";
+
+import Navigation from "./component/Navigation.jsx";
+import Masthead from "./component/masthead.jsx";
+import Iconsgrid from "./component/iconsgrid.jsx";
+import Imageshowcases from "./component/imageshowcases.jsx";
+import Testimonials from "./component/testimonials.jsx";
+import Calltoaction from "./component/calltoaction.jsx";
+import Footer from "./component/footer.jsx";
+
+
+
+
+export default function App() {
+  
+  return (
+    
+
+   
+
+<div>
+  
+
+<Navigation/>
+<Masthead/>
+<Iconsgrid/>
+ <Imageshowcases/>
+ <Testimonials/>
+ <Calltoaction/>
+ <Footer/>
+
+
+
+
+
+  
+  
+
+</div>
+
+
+
+
+  );
+}
+
---
+++ src/component/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function Navigation() {
+    return (
+       // <!-- Navigation-->
+        <nav class="navbar navbar-light bg-light static-top">
+            <div class="container">
+                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <a class="btn btn-primary" href="#signup">Sign Up</a>
+            </div>
+        </nav>
+
+        
+
+    );
+  }
